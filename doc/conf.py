491bfee6cc24f4bb356b917a9e0b3f0c43f0f6ca
Pablo Baudin <baudin.pablo@gmail.com>
2018-09-21 13:04:11 +0000

    Update conf.py with the possibility to use read the docs html theme

--- doc/conf.py
+++ doc/conf.py
@@ -96,7 +96,7 @@ add_module_names = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+# sphinx_rtd_theme = 'sphinx_rtd_theme'
 html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
