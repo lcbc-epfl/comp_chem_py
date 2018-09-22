96913d0719ca2ec5be49d41d9c83b06a655974d8
Pablo Baudin <baudin.pablo@gmail.com>
2018-09-22 09:52:12 +0200

    try fix latex

--- src/comp_chem_utils/spectrum.py
+++ src/comp_chem_utils/spectrum.py
@@ -465,7 +465,7 @@ def cross_section(xpts, ypts, unit_in='ENERGY: eV', temp=None, refraction=1.0):
         It is calculated from the spectral function as
 
         .. math::
-            \\sigma(\omega) = S (\omega) \cdot 
+            \\sigma(\omega) = S (\omega) \cdot \\text{temp\_effect} \cdot
 
         where the temp_effect comes from :py:func:`~comp_chem_utils.spectrum.temperature_effect`,
         ``SPEC_TO_SIGMA`` is the main conversion constant from :py:mod:`~comp_chem_utils.spectrum.conversions`,
