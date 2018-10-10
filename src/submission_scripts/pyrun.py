12a6093fea928b68c08264b610b5ae7f58c2babb
Pablo Baudin <baudin.pablo@gmail.com>
2018-10-10 15:06:35 +0200

    add global var to pyrun

--- src/submission_scripts/pyrun.py
+++ src/submission_scripts/pyrun.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-description="""Writes a bash scripts that will submit a calculation to scratch"""
+description="""Writes a bash scripts that will submit a calculation to scratch
+
+All the input arguments are global variables in this script!
+"""
 
 __author__="Pablo Baudin"
 __email__="pablo.baudin@epfl.ch"
@@ -9,7 +12,7 @@ import os
 import argparse
 
 from comp_chem_utils.utils import get_file_as_list
-from comp_chem_utils.program import executable, choose_exec, avail_progs
+from comp_chem_utils.program_info import executable, choose_exec, avail_progs
 from comp_chem_utils.sbatch_info import sbatch_info, sbatch_line
 
 
@@ -17,7 +20,7 @@ from comp_chem_utils.sbatch_info import sbatch_info, sbatch_line
 scr='/scratch/pbaudin'
 
 # assuming the submit_job.sh file is in the same dir as the current file
-script_dir = os.path.dirname(os.path.abspath(__file__)) 
+script_dir = os.path.dirname(os.path.abspath(__file__))
 script_name= '{}/submit_job.sh'.format(script_dir)
 
 def print_input(verbose=False):
@@ -26,10 +29,10 @@ def print_input(verbose=False):
 
 def read_command_line():
     """Read the submission line and split the arguments.
-    
+
     Every key is followed by its corresponding value(s).
     Possibilities include:
-    
+
     key value
     key=value
     key "value1 value2 ..."
@@ -151,26 +154,27 @@ def read_input():
     Based on input data, some options or actions are setup
     in case it fails, the usage documentation is printed.
     """
+    global PRG,JOB,VRS,INP,OUT,WRK,SCR,BSH,NOX,NOV,MAIL,SLUR,time,nodes,partition
     try:
         if sys.argv[1] in ['-h','--help']:
             show_usage()
-    
+
         elif sys.argv[1] == ('--avail'):
             avail_progs()
             sys.exit()
-    
+
         elif sys.argv[1] == ('--inp'):
             print_input()
             sys.exit()
-    
+
         elif sys.argv[1] == ('--INP'):
             print_input(verbose=True)
             sys.exit()
-    
+
         elif sys.argv[1] == ('-f'):
             # read from file
             pyrun_inp = get_file_as_list(sys.argv[2])
-            
+
             PRG = args['PRG'].read_file(pyrun_inp)
             JOB = args['JOB'].read_file(pyrun_inp)
             VRS = args['VRS'].read_file(pyrun_inp)
@@ -182,16 +186,16 @@ def read_input():
             NOX = args['NOX'].read_file(pyrun_inp)
             NOV = args['NOV'].read_file(pyrun_inp)
             MAIL = args['MAIL'].read_file(pyrun_inp)
-    
+
             SLUR = args['SLUR'].read_file(pyrun_inp)
             time = args['time'].read_file(pyrun_inp)
             nodes = args['nodes'].read_file(pyrun_inp)
             partition = args['partition'].read_file(pyrun_inp)
-    
+
         else:
             # read from command line
             lst = read_command_line()
-    
+
             PRG = args['PRG'].read_line(lst)
             JOB = args['JOB'].read_line(lst)
             VRS = args['VRS'].read_line(lst)
@@ -203,98 +207,99 @@ def read_input():
             NOX = args['NOX'].read_line(lst)
             NOV = args['NOV'].read_line(lst)
             MAIL = args['MAIL'].read_line(lst)
-    
+
             SLUR = args['SLUR'].read_line(lst)
             time = args['time'].read_line(lst)
             nodes = args['nodes'].read_line(lst)
             partition = args['partition'].read_line(lst)
-    
+
     except Exception as e:
         print e
         show_usage()
 
-    return PRG,JOB,VRS,INP,OUT,WRK,SCR,BSH,NOX,NOV,MAIL,SLUR,time,nodes,partition
-    
 
-def check_input(PRG,JOB,SCR,BSH,NOV):
+
+def check_input():
     """Some checks on input arguments"""
+    global PRG,SCR,BSH
 
     if not PRG:
         print """Compulsory keywords PRG not found in input file"""
         show_usage()
-    
+
     if not JOB:
         print """Compulsory keywords JOB not found in input file"""
         show_usage()
-    
+
     if not SCR:
         SCR = '{}/{}'.format(scr, JOB)
-    
+
     if not BSH:
         BSH = './submit_{}.sh'.format(JOB)
-    
+
     if PRG=='select':
         PRG = choose_exec()
     else:
         PRG = executable(PRG, version=VRS)
-    
-    return PRG,JOB,SCR,BSH,NOV
-    
 
-def print_input(BSH, PRG, JOB, WRK, SCR, INP, OUT, NOX, NOV, MAIL, SLUR):
+
+def print_param():
     """Print out input parameters to terminal"""
 
     print "BASH SCRIPT PARAMETERS:\n"
-    
+
     print "{:<26}: {}\n".format("Name of bash script", BSH)
-    
+
     print "{:<26}: {}/{}".format("Programme used", PRG.prog, PRG.version)
     print "{:<26}: {}".format("Jobname", JOB)
     print "{:<26}: {}".format("Working directory", WRK)
     print "{:<26}: {}\n".format("Scratch directory", SCR)
-    
+
     print "{:<26}: {}".format("Files copied to scratch", INP)
     print "{:<26}: {}\n".format("Files copied from scratch", OUT)
-    
+
     print "{:<26}: {}".format("Exectute bash script", not NOX)
     print "{:<26}: {}".format("Output printings", not NOV)
     print "{:<26}: {}".format("Send e-mail", MAIL)
     print "{:<26}: {}".format("Running on SLURM", SLUR)
-    # TODO print out sbatch info if SLUR
+    if SLUR:
+        print "   {:<23}: {}".format("Number of nodes", nodes)
+        print "   {:<23}: {}".format("Time limit", time)
+        print "   {:<23}: {}".format("Partition", partition)
 
 
-def create_submission_script(JOB, PRG, WRK, SCR, INP, OUT, MAIL):
+def create_submission_script():
     """Read, update, and return a template for a bash submission script"""
 
     template = get_file_as_list(script_name, raw=True)
 
     nlines = []
     for line in template:
-    
+
         # add current line to new bash script
         nlines.append( line.strip() )
-    
+
         # add new info to bash script
         if '# JOBNAME' in line:
             nlines.append( 'job={}'.format(JOB) )
-    
+
         elif '# PROG ENVIRONMENT' in line:
             nlines.extend ( PRG.get_env() )
-    
+
         elif '# WORK' in line:
             nlines.append( 'wrk={}'.format(WRK) )
-    
+
         elif '# SCRATCH' in line:
             nlines.append( 'scr={}'.format(SCR) )
-    
+
         elif ('# CP FILES TO SCR' in line):
             if INP:
                 nlines.append( 'cp -r {} $scr'.format( INP ) )
-    
+
         elif ('# RUN PROG' in line):
             nlines.append( 'cd $scr' )
             nlines.append( PRG.get_exec_line() )
-    
+
         elif ('# SAVE FILES FROM SCR' in line):
             if OUT:
                 nlines.append( 'out=$wrk/OUT_{}'.format(JOB) )
@@ -303,7 +308,7 @@ def create_submission_script(JOB, PRG, WRK, SCR, INP, OUT, MAIL):
                 nlines.append( 'cd $scr' )
                 nlines.append( 'cp -r {} $out'.format( OUT ) )
                 nlines.append( 'cd -' )
-    
+
         elif ('# SEND EMAIL' in line):
             if MAIL:
                 nlines.append( 'mail -s "$job is done on $hst" pablo.baudin@epfl.ch <<< $text' )
@@ -314,19 +319,19 @@ def create_submission_script(JOB, PRG, WRK, SCR, INP, OUT, MAIL):
 if __name__ == "__main__":
 
     # read input line or file
-    PRG,JOB,VRS,INP,OUT,WRK,SCR,BSH,NOX,NOV,MAIL,SLUR,time,nodes,partition = read_input()
+    read_input()
 
     # sanity check on input
-    PRG,JOB,SCR,BSH,NOV = check_input(PRG,JOB,SCR,BSH,NOV)
+    check_input()
 
     verbose = not NOV
 
     # print parameters
     if verbose:
-        print_input(BSH, PRG, JOB, WRK, SCR, INP, OUT, NOX, NOV, MAIL, SLUR)
+        print_param()
 
     # create submission script from input data
-    nlines = create_submission_script(JOB, PRG, WRK, SCR, INP, OUT, MAIL)
+    nlines = create_submission_script()
 
     # setup submission line
     if SLUR:
@@ -335,10 +340,10 @@ if __name__ == "__main__":
        sbatch_info['nodes'].set( nodes )
        sbatch_info['partition'].set( partition )
        sbatch = sbatch_line( BSH )
-    
+
     else:
        sbatch = 'nohup {0} > {1}.stdo 2> {1}.stde &'.format(BSH, JOB)
-    
+
     if verbose:
         print '\nSubmission line:'
         print sbatch
@@ -347,7 +352,7 @@ if __name__ == "__main__":
     with open(BSH,'w') as script:
         for line in nlines:
             script.write('{}\n'.format(line))
-    
+
         # add submission line as comment at the end of the file
         script.write("# To submit this batch script use the following command:\n")
         script.write("# {}\n".format(sbatch))
@@ -356,5 +361,5 @@ if __name__ == "__main__":
     os.system('chmod u+x '+BSH)
     if not NOX:
         os.system(sbatch)
-    
+
 
