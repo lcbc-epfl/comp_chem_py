922f96ff7cbcb8e6ee2969d15d07967a30b0461a
Pablo Baudin <baudin.pablo@gmail.com>
2019-02-05 15:32:23 +0100

    remove blanks

--- src/cpmd_scripts/truncate_solvent.py
+++ src/cpmd_scripts/truncate_solvent.py
@@ -24,7 +24,7 @@ def read_xyz_structure(xyz, nat_solute, nat_solvent):
     """decompose xyz data as solute + group of solvent molecules"""
     xyz_struc = []
     xyz_struc.append(xyz[0:nat_solute])
-    
+
     nlines = len(xyz)
     xyz_struc += [ xyz[x:x+nat_solvent] for x in xrange(nat_solute, nlines, nat_solvent) ]
 
@@ -61,7 +61,7 @@ if __name__ == '__main__':
           H ...
           :
           """)
-    
+
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("NATQM", type=int, help="Number of atoms in QM region or in solute")
     parser.add_argument("--xyz_inp", type=str, help="Input name for trajectory file in xyz format", default='TRAJEC.xyz')
@@ -71,50 +71,50 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if not args.xyz_out:
         args.xyz_out = '{}_{}.xyz'.format(args.xyz_inp[:-4], args.NMOLMM)
-    
+
     # print info
     print 'Input trajectory read from  : {}'.format(args.xyz_inp)
     print 'Output trajectory printed to: {}\n'.format(args.xyz_out)
     print 'Number of atoms in QM region or in solute: {}'.format(args.NATQM)
     print 'Number of atoms per solvent molecules    : {}'.format(args.NATMM)
     print 'Number of solvent molecules to keep      : {}\n'.format(args.NMOLMM)
-    
+
     # read trajectory file
     steps, traj_xyz = read_TRAJEC_xyz(args.xyz_inp)
-    
+
     print 'Number of structures read from input     : {}\n'.format(len(traj_xyz))
-    
+
     # get truncated xyz data for each xyz geometry in trajectory
     new_traj = []
     super_max_dist = 0.0
     for xyz in traj_xyz:
         # reference point is taken from COM of solute
-        ref = center_of_mass(xyz[0:args.NATQM]) 
-    
+        ref = center_of_mass(xyz[0:args.NATQM])
+
         # convert xyz data in block structure (solute and solvent)
         xyz_struc = read_xyz_structure(xyz, args.NATQM, args.NATMM)
-    
+
         # associate a distance to each block
         dist = get_distance(xyz_struc, ref)
-    
+
         # get the distance for the farthest molecule to be included
-        max_dist = sorted(dist)[args.NMOLMM] 
+        max_dist = sorted(dist)[args.NMOLMM]
         if max_dist > super_max_dist:
             super_max_dist = max_dist
-    
+
         # remove all blocks with distance larger than max_dist
         new_struc = []
         for d, b in zip(dist, xyz_struc):
             if d<=max_dist:
                 new_struc.append(b)
-    
+
         # remove block structure
         new_traj.append( [atom for block in new_struc for atom in block] )
-        
-    
+
+
     # write new truncated trajectory file
     write_TRAJEC_xyz(steps, new_traj, output=args.xyz_out)
-    
+
     print "Maximum distance from solute center of mass: {} AA\n".format(max_dist)
     print "New trajectory printed to {} file".format(args.xyz_out)
 
