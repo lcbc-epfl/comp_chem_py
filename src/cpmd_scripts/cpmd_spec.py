8b39d0733afad8633bf3e7f358b70b520b771e77
Pablo Baudin <baudin.pablo@gmail.com>
2019-02-07 15:39:06 +0100

    clean out cpmd_spec script

--- src/cpmd_scripts/cpmd_spec.py
+++ src/cpmd_scripts/cpmd_spec.py
@@ -1,41 +1,50 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 18 11:49:30 2019
+#!/usr/bin/env python
+"""Simple script to plot absorption spectra from CPMD TDDFT output files."""
 
-@author: pbaudin
-"""
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 import comp_chem_utils.spectrum as sp
 
-flist = sys.argv[1:]
 
-exc = np.empty(0)
-osc = np.empty(0)
+# TRY TO READ LIST OF OUTPUT FILES
+try:
+    flist = sys.argv[1:]
+    if not flist:
+        raise Exception('We need some parameters...')
+except:
+    print(__doc__)
+    sys.exit("""
+    Usage: 
+
+        python cpmd_spec.py cpmd1.out cpmd2.out ...
+
+    Where cpmdx.out denote a CPMD output file for a TDDFT calculation.
+    At least one output file should be given.""")
+
+
+# READ THE OUTPUT FILES AND EXTRACT SPECTRUM INFORMATION
 exc = []
 osc = []
-# read all files
 for filename in flist:
     e, f = sp.read_spectrum(filename,"cpmd")
-    #exc = np.concatenate( (exc, e) )
-    #osc = np.concatenate( (osc, f) )
     exc.append(e)
     osc.append(f)
-    
-#sp.plot_spectrum(exc, osc)
-xpts =[]
-ypts = []
-# collect spectra
+
+
+# FOR EACH SET OF DATA:
+#   - PLOT THE ABS. CROSS SECTION
+#   - PLOT A STICK SPECTRUM
 for i,(e,f) in enumerate(zip(exc, osc)):
     x, y = sp.plot_spectrum(e,f,plot=False)
-    xpts.append(x)
-    ypts.append(y)
     tmp = plt.plot(x, y, label=flist[i])
     plt.vlines(e, -0.1, f, color=tmp[0].get_color())
-    
+ 
+
+# SET UP THE PLOT AND SHOW IT
+plt.ylabel('Cross section [$\\AA^2 \\cdot $ molecules$^{-1}$]')
+plt.xlabel('Transition energy [eV]')
 plt.legend()
 plt.show()
     
